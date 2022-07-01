668ff5adfa8868084f2da8fbb3ee420558629dd5
Roger Torres <torresroger445@gmail.com>
2022-07-01 13:14:26 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Roger Torres on 01/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
